cf3d49e86f3c9330e2b259f74171fdfa8f475f33
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-07-09 15:12:16 +0200

    Lint

--- src/main/store.ts
+++ src/main/store.ts
@@ -8,5 +8,5 @@ export const getStore = () => store;
 if (store.get('browsing.defaultWebpage') === undefined)
   store.set('browsing.defaultWebpage', 'https://www.google.com');
 
-  if (store.get('browsing.searchEngine') === undefined)
+if (store.get('browsing.searchEngine') === undefined)
   store.set('browsing.searchEngine', 'google');
