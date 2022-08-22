c7c5c696fbebd3b1f5bccd5b2dfcc7d0979e85d4
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-08-22 14:05:15 +0200

    Fix tsc

--- src/__tests__/Popup.test.tsx
+++ src/__tests__/Popup.test.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '@testing-library/jest-dom';
-import { act, fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { Middleware } from '@reduxjs/toolkit';
