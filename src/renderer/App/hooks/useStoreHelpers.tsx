f2b9fd9d998705dbf34670f5206b3a232f9815ee
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-08-22 14:07:34 +0200

    Fix Board

--- src/renderer/App/hooks/useStoreHelpers.tsx
+++ src/renderer/App/hooks/useStoreHelpers.tsx
@@ -25,6 +25,9 @@ import { BrowserProps } from 'renderer/App/components/Browser/Types';
 import { useBoard } from './useBoard';
 import { useBrowserMethods } from './useBrowserMethods';
 
+const DEFAULT_HEIGHT = 800;
+const DEFAULT_WIDTH = 600;
+
 export const useStoreHelpers = (helpersParams?: { boardId?: string }) => {
   const dispatch = useAppDispatch();
   const board = useBoard();
@@ -78,10 +81,11 @@ export const useStoreHelpers = (helpersParams?: { boardId?: string }) => {
           break;
       }
 
-      console.log({width, height})
-
       if (boardContainer)
         width = Math.min(boardContainer?.clientWidth - 20, width);
+
+      height = height || DEFAULT_HEIGHT;
+      width = width || DEFAULT_WIDTH;
 
       const { x, y } = getCoordinateWithNoCollision(
         document,
