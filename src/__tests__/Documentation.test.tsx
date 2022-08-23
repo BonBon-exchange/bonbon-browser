ecc325ff4a116a8401d613a9f4c48857ba942ca1
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-08-23 17:47:42 +0200

    Add Documentation snapshot

--- src/__tests__/Documentation.test.tsx
+++ src/__tests__/Documentation.test.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '@testing-library/jest-dom';
-import { render, act, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { Middleware } from '@reduxjs/toolkit';
+import renderer, { act } from 'react-test-renderer';
 
 import { Documentation } from '../renderer/App/components/Documentation';
 import { mockWindow } from './beforeAll';
 import { initialState } from '../renderer/App/store/reducers/Board';
 
+let tree: any;
 let store: any;
 const middlewares: Middleware[] = [];
 
@@ -24,13 +26,15 @@ describe('Documentation', () => {
   });
 
   it('should render', () => {
-    expect(
-      render(
+    act(() => {
+      tree = renderer.create(
         <Provider store={store}>
           <Documentation {...props} />
         </Provider>
-      )
-    ).toBeTruthy();
+      );
+    });
+
+    expect(tree).toMatchSnapshot();
   });
 
   it('should show Webpages windows', async () => {
