b2de998e8fb29d18d37f5c1fbaf510e646345153
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-08-23 17:40:29 +0200

    Add BrowserTopBar snapshot

--- src/__tests__/BrowserTopBar.test.tsx
+++ src/__tests__/BrowserTopBar.test.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Middleware } from '@reduxjs/toolkit';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import renderer, { act } from 'react-test-renderer';
 
 import { BrowserTopBar } from '../renderer/App/components/BrowserTopBar';
 import { initialState } from '../renderer/App/store/reducers/Board';
 import { mockWindow } from './beforeAll';
 
+let tree: any;
 let store: any;
 const middlewares: Middleware[] = [];
 
@@ -30,12 +31,14 @@ describe('BrowserTopBar', () => {
   });
 
   it('should render', () => {
-    expect(
-      render(
+    act(() => {
+      tree = renderer.create(
         <Provider store={store}>
           <BrowserTopBar {...browserTopBarProps} />
         </Provider>
-      )
-    ).toBeTruthy();
+      );
+    });
+
+    expect(tree).toMatchSnapshot();
   });
 });
