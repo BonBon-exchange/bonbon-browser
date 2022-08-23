8a31f85e69656e814f1b9543780790a5fad508c8
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-08-23 18:03:06 +0200

    Add Popup snapshot

--- src/__tests__/Popup.test.tsx
+++ src/__tests__/Popup.test.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { Middleware } from '@reduxjs/toolkit';
+import renderer, { act } from 'react-test-renderer';
 
 import { Popup } from '../renderer/App/components/Popup';
 import { mockWindow } from './beforeAll';
 import { initialState } from '../renderer/App/store/reducers/Board';
 
+let tree: any;
 let store: any;
 const middlewares: Middleware[] = [];
 
@@ -25,14 +26,16 @@ describe('Library', () => {
   });
 
   it('should render', () => {
-    expect(
-      render(
+    act(() => {
+      tree = renderer.create(
         <Provider store={store}>
           <Popup {...popupProps}>
             <>Hello</>
           </Popup>
         </Provider>
-      )
-    ).toBeTruthy();
+      );
+    });
+
+    expect(tree).toMatchSnapshot();
   });
 });
