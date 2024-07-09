71d7169f1cc0243fad4faead3cd637c4ba09959e
Daniel Febrero <dfebrero@glyph.technology>
2024-07-09 09:34:38 +0200

    erb-scripts: globally install ts-node in a subprocess of one step of workflow

--- .erb/scripts/run-check-native-dep.js
+++ .erb/scripts/run-check-native-dep.js
@@ -1,9 +1,12 @@
 const { execSync } = require('child_process');
 
 try {
-  execSync('node --loader ts-node/esm ./.erb/scripts/check-native-dep.mjs', {
-    stdio: 'inherit',
-  });
+  execSync(
+    'npm install -g ts-node && node --loader ts-node/esm ./.erb/scripts/check-native-dep.mjs',
+    {
+      stdio: 'inherit',
+    }
+  );
 } catch (error) {
   console.error('Failed to run check-native-dep script:', error);
   process.exit(1);
