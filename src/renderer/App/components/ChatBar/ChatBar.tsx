038c22fdcfbf9dfd54406a71e8d840b3e0ca0a5d
Daniel Febrero <dfebrero@glyph.technology>
2024-07-09 22:47:59 +0200

    chat: animate ChatBar when user is getting close to it

--- src/renderer/App/components/ChatBar/ChatBar.tsx
+++ src/renderer/App/components/ChatBar/ChatBar.tsx
@@ -1,16 +1,26 @@
 import clsx from 'clsx';
 
 import './style.scss';
-import { useCallback, useState, useEffect } from 'react';
+import { useCallback, useState, useEffect, useRef } from 'react';
 
 export default () => {
+  const chatBarRef = useRef<HTMLDivElement>(null);
   const [isStateMessageReceived, setIsStateMessageReiceived] =
     useState<boolean>(false);
-
+  const [shouldEnhighChatbar, setShouldEnhighChatbar] =
+    useState<boolean>(false);
   const messageReceivedAction = useCallback(() => {
     setIsStateMessageReiceived(true);
     setTimeout(() => setIsStateMessageReiceived(false), 300);
   }, [setIsStateMessageReiceived]);
+
+  const mouseMoveAction = useCallback((e: MouseEvent) => {
+    const cb = chatBarRef.current?.getBoundingClientRect();
+    const distance = Number(cb?.top) - e.clientY;
+    distance < 100
+      ? setShouldEnhighChatbar(true)
+      : setShouldEnhighChatbar(false);
+  }, []);
 
   useEffect(() => {
     window.app.listener.chatMessageReceived(messageReceivedAction);
@@ -19,10 +29,21 @@ export default () => {
     };
   }, [messageReceivedAction]);
 
+  useEffect(() => {
+    window.addEventListener('mousemove', mouseMoveAction);
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveAction);
+    };
+  });
+
   return (
     <div
       id="chat-bar"
-      className={clsx({ 'message-received': isStateMessageReceived })}
+      ref={chatBarRef}
+      className={clsx({
+        'message-received': isStateMessageReceived,
+        'user-is-close': shouldEnhighChatbar,
+      })}
     />
   );
 };
