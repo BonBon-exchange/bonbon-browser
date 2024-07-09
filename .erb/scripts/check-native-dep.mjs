5832495df3cf6b8b84d9fab5bccfc25a5dc194be
Daniel Febrero <dfebrero@glyph.technology>
2024-07-09 09:17:10 +0200

    erb-scripts: light changes recommende by gpt-4o

--- .erb/scripts/check-native-dep.mjs
+++ .erb/scripts/check-native-dep.mjs
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import { execSync } from 'child_process';
+import chalk from 'chalk'; // You can now use import directly
 import { dependencies } from '../../package.json';
 
 (async function checkNativeDeps() {
   if (dependencies) {
-    const chalk = (await import('chalk')).default;
     const dependenciesKeys = Object.keys(dependencies);
     const nativeDeps = fs
       .readdirSync('node_modules')
@@ -13,9 +13,6 @@ import { dependencies } from '../../package.json';
       process.exit(0);
     }
     try {
-      // Find the reason for why the dependency is installed. If it is installed
-      // because of a devDependency then that is okay. Warn when it is installed
-      // because of a dependency
       const { dependencies: dependenciesObject } = JSON.parse(
         execSync(`npm ls ${nativeDeps.join(' ')} --json`).toString()
       );
@@ -53,4 +50,4 @@ import { dependencies } from '../../package.json';
       console.log('Native dependencies could not be checked');
     }
   }
-})()
+})();
