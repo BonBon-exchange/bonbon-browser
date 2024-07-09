b8b5aa0c003b54056f2f232eff802388b57c0ece
Daniel Febrero <dfebrero@glyph.technology>
2024-07-09 09:54:20 +0200

    scripts: custom step for linux

--- .erb/scripts/run-check-native-dep.sh
+++ .erb/scripts/run-check-native-dep.sh
@@ -3,8 +3,13 @@
 # Ensure ts-node is installed
 npm install ts-node
 
-# Run the check-native-dep.mjs script with the required loader
-npx ts-node-esm ./.erb/scripts/check-native-dep.mjs
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  npm install ts-node-esm --legacy-peer-deps
+  # Run the check-native-dep.mjs script with the required loader
+  ts-node-esm ./.erb/scripts/check-native-dep.mjs
+else
+  ts-node ./.erb/scripts/check-native-dep.js
+fi
 
 # Check the exit status of the previous command and exit if it failed
 if [ $? -ne 0 ]; then
