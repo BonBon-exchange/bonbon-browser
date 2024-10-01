b158121451c97365648bb277964eab77b09eddfc
danny bengal <dannybengal@cdxxotus.com>
2024-10-02 00:47:47 +0200

    autoupdate not working on macos

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,8 +8,8 @@
  * When running `npm run build` or `npm run build:main`, this file is compiled to
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
-import { app, dialog } from 'electron';
-import { autoUpdater } from 'electron-updater';
+import { app } from 'electron';
+// import { autoUpdater } from 'electron-updater';
 
 import './appEvents';
 import './logger';
@@ -19,32 +19,32 @@ if (app.isPackaged) {
   sourceMapSupport.install();
 }
 
-app.on('ready', () => {
-  // Check for updates and notify the user
-  autoUpdater.checkForUpdatesAndNotify();
-});
+// app.on('ready', () => {
+//   // Check for updates and notify the user
+//   autoUpdater.checkForUpdatesAndNotify();
+// });
 
-autoUpdater.on('update-available', () => {
-  dialog.showMessageBox({
-    type: 'info',
-    title: 'Update Available',
-    message: 'A new version is available. Downloading now...',
-  });
-});
+// autoUpdater.on('update-available', () => {
+//   dialog.showMessageBox({
+//     type: 'info',
+//     title: 'Update Available',
+//     message: 'A new version is available. Downloading now...',
+//   });
+// });
 
-autoUpdater.on('update-downloaded', () => {
-  const options = {
-    type: 'info' as const,
-    buttons: ['Restart', 'Later'],
-    title: 'Update Available',
-    message: 'A new update is available. Restart to apply the update?',
-  };
-  dialog
-    .showMessageBox(options)
-    .then((result) => {
-      if (result.response === 0) {
-        autoUpdater.quitAndInstall();
-      }
-    })
-    .catch(console.log);
-});
+// autoUpdater.on('update-downloaded', () => {
+//   const options = {
+//     type: 'info' as const,
+//     buttons: ['Restart', 'Later'],
+//     title: 'Update Available',
+//     message: 'A new update is available. Restart to apply the update?',
+//   };
+//   dialog
+//     .showMessageBox(options)
+//     .then((result) => {
+//       if (result.response === 0) {
+//         autoUpdater.quitAndInstall();
+//       }
+//     })
+//     .catch(console.log);
+// });
