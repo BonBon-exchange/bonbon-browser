7486b2c4354edebf70df4dc45510c7c26285ad27
danny bengal <dannybengal@cdxxotus.com>
2024-10-01 14:13:06 +0200

    autoupdate + flatpak

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,7 +8,8 @@
  * When running `npm run build` or `npm run build:main`, this file is compiled to
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
-import { app } from 'electron';
+import { app, dialog } from 'electron';
+import { autoUpdater } from 'electron-updater';
 
 import './appEvents';
 import './logger';
@@ -17,3 +18,33 @@ if (app.isPackaged) {
   const sourceMapSupport = require('source-map-support');
   sourceMapSupport.install();
 }
+
+app.on('ready', () => {
+  // Check for updates and notify the user
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+autoUpdater.on('update-available', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update Available',
+    message: 'A new version is available. Downloading now...',
+  });
+});
+
+autoUpdater.on('update-downloaded', () => {
+  const options = {
+    type: 'info' as const,
+    buttons: ['Restart', 'Later'],
+    title: 'Update Available',
+    message: 'A new update is available. Restart to apply the update?',
+  };
+  dialog
+    .showMessageBox(options)
+    .then((result) => {
+      if (result.response === 0) {
+        autoUpdater.quitAndInstall();
+      }
+    })
+    .catch(console.log);
+});
